99f248a03629ccc48bd22ccc218fc8d8bbed285c
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-24 19:15:19 +0200

    building new issue page

--- app/Issues/new/page.tsx
+++ app/Issues/new/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button, TextArea, TextField} from "@radix-ui/themes";
 
 const NewIssue = () => {
-  return <div>NewIssue </div>;
+  return( 
+  <div className="max-w-lg space-y-4 mt-5">
+    <TextField.Root placeholder= 'Title...'/>
+    <TextArea placeholder="Reply to comment..."/>
+    <Button>Submit Issue</Button>
+      
+    
+  </div>
+)
 };
 
 export default NewIssue;
--- app/Issues/view/page.tsx
+++ app/Issues/view/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import {Button } from "@radix-ui/themes"
+import Link from "next/link";
+import classNames from "classnames";
 
 const IssuePage = () => {
-  return <div>IssuePage</div>;
-};
+  return (
+    <div className='my-4'>
+      <Button asChild size="3"><Link href="/issues/new">Create New Issue</Link></Button>
+    </div>
+  )
+}
 
-export default IssuePage;
+export default IssuePage
