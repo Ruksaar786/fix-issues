aaaa117d027f9ff7439d4ed0afdde2ebdf8bab07
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-24 19:30:00 +0200

    add smiple mde editor

--- app/Issues/new/page.tsx
+++ app/Issues/new/page.tsx
@@ -1,11 +1,14 @@
+'use client'
 import React from "react";
-import { Button, TextArea, TextField} from "@radix-ui/themes";
+import { Button, TextField} from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssue = () => {
   return( 
   <div className="max-w-lg space-y-4 mt-5">
     <TextField.Root placeholder= 'Title...'/>
-    <TextArea placeholder="Reply to comment..."/>
+    <SimpleMDE placeholder="Reply to comment..."/>
     <Button>Submit Issue</Button>
       
     
