85e7761c37c7555a807a0181dbe47c858b1f409e
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-24 17:52:53 +0200

    build api

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# DATABASE_URL="postgresql://postgres:786@localhost:5432/fix-issue?schema=public"
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- app/api/issues/new/route.ts
+++ app/api/issues/new/route.ts
@@ -1,20 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import {z} from 'zod';
 
-//http://localhost:3000/api/issues/new
+// http://localhost:3000/api/issues/new
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-  if (!body) {
-    return NextResponse.json({ error: "No data found" }, { status: 400 });
-  }
+const IssueFormSchema = z.object({
+    title:z.string().min(1, {message:"Title is required"}).max(50),
+    description:z.string().min(6, {message:"Description is required"}).max(2000)
+}) 
 
-  const issue = prisma.issue.create({
-    data: {
-      title: body.title,
-      desccription: body.description,
-    },
-  });
 
-  return NextResponse.json(issue, {status:201})
+export async function POST(request:NextRequest){
+const body = await request.json();
+const validation = IssueFormSchema.safeParse(body)
+
+if(!validation.success){
+    return NextResponse.json(validation.error.format(), {status:400})
+}
+
+const issue = await prisma.issue.create({
+    data:{
+        title:body.title,
+        description:body.description,
+    }
+})
+return NextResponse.json(issue, {status:201})
 }
