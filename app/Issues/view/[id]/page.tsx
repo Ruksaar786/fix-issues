b9b41adf8ce50c655c4a6df8e42369944bac0433
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-05-18 19:44:12 +0200

    updated assign user schema

--- app/Issues/view/[id]/page.tsx
+++ app/Issues/view/[id]/page.tsx
@@ -1,10 +1,13 @@
 import prisma from '@/prisma/client'
 import { Box, Grid, Flex } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 import EditIssueButton from '../_components/EditIssueButton'
 import DeleteIssueButton from '../_components/DeleteIssueButton'
 import IssueDetails from '../_components/IssueDetails'
+import { auth } from '@/auth'
+import AssigneeSelect from '../_components/AssigneeSelect'
 const IssueDetailsPage = async({params}:{params:{id:string}}) => {
+    const session = await auth();
     let issue = await prisma.issue.findUnique({
         where:{
             id:params.id
@@ -18,8 +21,16 @@ const IssueDetailsPage = async({params}:{params:{id:string}}) => {
     </Box>
     <Box>
         <Flex direction="column" gap="4">
-<EditIssueButton IssueId={issue.id} />
+            <AssigneeSelect issue={issue} />
+            {session?.user && (
+                <>
+                <EditIssueButton IssueId={issue.id} />
 <DeleteIssueButton IssueId={issue.id} />
+</>
+            )}
+
+{!session?.user && redirect('/api/auth/signin?callbackUrl=/issues/view') }
+
 </Flex>
     </Box>
     </Grid>
--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,22 +1,56 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { describe } from "node:test";
-import { IssueFormSchema } from '../../../ValidationSchema'
+import { patchIssueSchema } from "@/app/ValidationSchema";
 
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const validation = IssueFormSchema.safeParse(body)
+export async function PATCH(request:NextRequest,{params}:{params:{id:string}}){
+const body = await request.json();
+const validation = patchIssueSchema.safeParse(body)
 
-  if (!validation.success) {
-    return NextResponse.json(validation.error.format(), { status: 400 });
+if(!validation.success){
+    return NextResponse.json(validation.error.format(), {status:400})
+}
+
+const {title, description, assignedToUserId} = body;
+
+if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId }
+    })
+    if (!user)
+      return NextResponse.json({ error: 'Invalid user' }, { status: 400 })
   }
 
-  const issue = await prisma.issue.create({
-    data: {
-      title:body.title,
-      description:body.description,
-    },
-  });
 
-  return NextResponse.json(issue, { status: 201 });
+const issue = await prisma.issue.findUnique({
+    where:{id:params.id}
+})
+
+if(!issue){
+    return NextResponse.json({error:"No issue found"}, {status:400}) 
+}
+const Updatedissue = await prisma.issue.update({
+    where:{id:issue.id},
+    data:{
+        title,
+        description,
+        assignedToUserId
+    }
+})
+return NextResponse.json(Updatedissue, {status:201})
+}
+
+
+export async function DELETE(request:NextRequest,{params}:{params:{id:string}}){
+    const issue = await prisma.issue.findUnique({
+        where:{id:params.id}
+    })
+    
+    if(!issue){
+        return NextResponse.json({error:"No issue found"}, {status:404}) 
+    }
+
+    await prisma.issue.delete({
+        where:{id:issue.id}
+    })
+    return NextResponse.json({}, {status:200}) 
 }
