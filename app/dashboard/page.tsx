a2baf53d03c71e8e614e2523750098489f9ef1b0
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-05-22 08:31:09 +0200

    components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,28 @@
-import Pagination from "../issues/view/_components/Pagination";
-const Dashboard = () => {
- return (
-<div>
-  <Pagination itemCount={100} pageSize={10} currentPage={2} />
-</div>
- )
+import React from 'react'
+import LatestIssue from './_components/LatestIssue'
+import IssueSummary from './_components/IssueSummary'
+import prisma from '@/prisma/client'
+import IssueChart from './_components/IssueChart'
+import { Grid, Flex } from '@radix-ui/themes'
+import { Metadata } from 'next'
+
+export const metadata:Metadata = {
+  title:"Issue Dashboard",
+  description:"Issue dashboard, status card, status bar graph, latest issue"
+}
+const Dashboard = async () => {
+  const open = await prisma.issue.count({where:{status:'OPEN'}})
+  const inProgress = await prisma.issue.count({where:{status:'IN_PROGRESS'}})
+  const closed = await prisma.issue.count({where:{status:'CLOSED'}})
+  return (
+    <Grid columns={{initial:'1', md:"2"}} gap="5">
+      <Flex direction="column" gap="5">
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <IssueChart open={open} inProgress={inProgress} closed={closed}/>
+      </Flex>
+      <LatestIssue />
+      </Grid>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
--- prisma/client.ts
+++ prisma/client.ts
@@ -1,15 +1,33 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  return new PrismaClient()
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-export default prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+async function createIssue() {
+  const issue = await prisma.issue.create({
+    data: {
+      title: 'Issue',
+      description: 'This is a sample issue description',
+    },
+  });
+  console.log('Issue created:', issue);
+}
+
+createIssue()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
